c818ee30e3147d73676a6c63a0b4a4bd095f227b
harshita <harshita.mahehswari@glsuniversity.ac.in>
2025-07-18 10:57:25 +0530

    July18

---
+++ operators.js
@@ -0,0 +1,20 @@
+// let a= 5
+// let b = 2
+// let result = a<b || "ok"
+// console.log(result)
+
+//number strig boolean null undefined
+let user //undefined //undefined or null
+let loginuser = user ?? "Guest" //nullish coalescing operator
+console.log(`Welcome ${loginuser}`)
+
+
+
+
+
+
+
+
+
+
+
---
+++ variableandfun.js
@@ -0,0 +1,9 @@
+let myFun =()=>{
+    let radius = 2
+    // const PI = 3.14
+    // let area = PI * radius * radius
+    // alert("Area of radius" + radius + " circle is: " + area)
+
+    let area = Math.PI * radius * radius
+    alert(`Area of radius ${radius}  circle is:  ${area}`) //template literal ES6
+}
